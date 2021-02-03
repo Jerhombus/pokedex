254737ebd07c464bab9943b34146781382ec9ed2
Jerome Copeland <76133780+Jerhombus@users.noreply.github.com>
2021-02-03 14:12:34 -0700

    firs commit

--- poke.py
+++ poke.py
@@ -1,15 +1,12 @@
 import json
 import requests
 
-
 class Pokemon:
-    list_of_pokemon = []
-
-    def __init__(self, name=None, type_Of=None, abilities=None, weight=None)
-    self.name = name
-    self.type_Of = type_Of
-    self.abilities = abilities
-    self.weight = weight
+    def __init__(self, name, type_Of, abilities, weight):
+        self.name = name
+        self.type_Of = type_Of
+        self.abilities = abilities
+        self.weight = weight
 
     def get_name(self):
         return self.name
@@ -29,31 +26,41 @@ class Pokemon:
     def __str__(self):
         return f'{self.type_Of} {self.name}'
 
-    @classmethod
+    # @classmethod
     def all(self):
-        for d for d in self.list_of_pokemon:
-            print(d)
+        print("+:"*25)
+        print(f'Pokemon name: {(self.name).title()}')
+        print(f'Pokemon Type: {(self.type_Of).title()}')
+        print(f'Pokemon ability: {(self.abilities).title()}')
+        print(f'Pokemon weight: {self.weight}')
+        print(":+"*25)
 
 
 class PokeDex:
-    def __init__(self, name=None):
-        self.pokemon_Name = name
+    def __init__(self, name):
         self.web_link = f'https://pokeapi.co/api/v2/pokemon/{name}'
 
-    def run(self, url):
+    def run(self):
         url = requests.get(self.web_link)
-        if urlstatus_code == 400:
+        if url.status_code == 400:
             print('You must pass in a valid Poke Name to execute this program.')
         elif url.status_code == 200:
-            data = url.json()['MRData']['StandingsTable']['StandingsList'][0]['DriverStandings']:
-            for Pokedex in data:
-                Driver(driver['Driver']['givenName'], driver['Driver']['familyName'], type=driver['Driver']['dateofBirth'], driver['points'], driver['position'], driver['Driver']['nationality'], driver['constructors'][0])
-        PokeDex.all()
+            data = url.json()
+            abilities = data['abilities'][0]['ability']['name']
+            name = data['forms'][0]['name']
+            types = data['types'][0]['type']['name']
+            weight = data['weight']
+            p = Pokemon(name, types, abilities, weight)
+            p.all()
 
 # data = res.json()['MRData']['StandingsTable']['StandingsList'][0]['DriverStandings']:
 #    for driver in data:
 #        d = Driver(driver['Driver']['givenName'], driver['Driver']['familyName'], type=driver['Driver']['dateofBirth'], driver['points'], driver['position'], driver['Driver']['nationality'], driver['constructors'][0])
 
 
-program = Ergast(1975)
-program.run()
+p_list = ['ratata', 'pikachu', 'charmander', 'magikarp', 'ditto', 'snorlax', 'eevee', 'dragonite', 'koffing', 'weezing',
+          'moltres', 'kadabra', 'articuno', 'ghastly', 'ekans', 'meowth', 'lucario', 'bulbasaur', 'articuno', 'zapdos', 'mew']
+
+for i in p_list:
+    program = PokeDex(i)
+    program.run()
